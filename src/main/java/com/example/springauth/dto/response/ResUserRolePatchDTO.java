d3fc72ff5cc710fa76a7443ba4c358d3ce315804
dowon0113 <dowon1235@naver.com>
2025-05-15 22:13:45 +0900

    feat: 관리자 권한 변경 api 응답 DTO 생성

---
+++ src/main/java/com/example/springauth/dto/response/ResUserRolePatchDTO.java
@@ -0,0 +1,32 @@
+package com.example.springauth.dto.response;
+
+import com.example.springauth.domain.user.UserEntity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class ResUserRolePatchDTO {
+
+    private String username;
+    private String nickname;
+    private List<RoleDTO> roles;
+
+    public static ResUserRolePatchDTO of(UserEntity user) {
+        return ResUserRolePatchDTO.builder()
+                .username(user.getUsername())
+                .nickname(user.getNickname())
+                .roles(List.of(new RoleDTO(user.getRoleType().name())))
+                .build();
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class RoleDTO {
+        private String role;
+    }
+}
