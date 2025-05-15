4eed3de8bf55458dfc6178bb7bcf49c189778d66
dowon0113 <dowon1235@naver.com>
2025-05-15 15:01:18 +0900

    feat: 로그인 기능 구현 및 JWT 기반 응답 DTO 생성 메서드 추가
    - 사용자의 username과 password를 검증하여 로그인 처리
      - 존재하지 않는 사용자일 경우 USER_NOT_FOUND 예외 발생
      - 비밀번호가 일치하지 않으면 INVALID_CREDENTIALS 예외 발생
    
    - 로그인 성공 시 JwtUtil을 이용해 accessToken 생성
    - UserEntity와 JwtUtil을 기반으로 JWT 응답을 생성하는 ResLoginDTO.of() 정적 메서드 구현
    - 로그인 결과는 accessToken을 포함한 ResLoginDTO로 반환

--- src/main/java/com/example/springauth/service/UserService.java
+++ src/main/java/com/example/springauth/service/UserService.java
@@ -3,27 +3,48 @@ package com.example.springauth.service;
 import com.example.springauth.common.exception.CustomException;
 import com.example.springauth.common.exception.GlobalExceptionCode;
 import com.example.springauth.domain.user.UserEntity;
+import com.example.springauth.dto.request.ReqLoginDTO;
 import com.example.springauth.dto.request.ReqUserPostDTO;
+import com.example.springauth.dto.response.ResLoginDTO;
 import com.example.springauth.dto.response.ResUserPostDTO;
+import com.example.springauth.jwt.JwtUtil;
 import com.example.springauth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final UserRepository userRepository;
+    private final JwtUtil jwtUtil;
 
     public ResUserPostDTO save(ReqUserPostDTO dto){
         if (userRepository.existsByUsername(dto.getUsername())) {
             throw new CustomException(GlobalExceptionCode.USER_ALREADY_EXISTS);
         }
 
-        UserEntity user = dto.toEntity();
+        // ID 수동 생성
+        Long newId = UUID.randomUUID().getMostSignificantBits() & Long.MAX_VALUE;
+
+        UserEntity user = dto.toEntity(newId);
 
         userRepository.save(user);
 
         return ResUserPostDTO.of(user);
+    }
+
+    public ResLoginDTO login(ReqLoginDTO dto) {
+        UserEntity user = userRepository.findByUsername(dto.getUsername())
+                .orElseThrow(() -> new CustomException(GlobalExceptionCode.USER_NOT_FOUND));
+
+        if (!user.getPassword().equals(dto.getPassword())) {
+            throw new CustomException(GlobalExceptionCode.INVALID_CREDENTIALS);
+        }
+
+        return ResLoginDTO.of(user, jwtUtil);
     }
 }
