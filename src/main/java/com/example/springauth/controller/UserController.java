dbc70aa7debca400716bd30e9314c3856c8aeecd
dowon0113 <dowon1235@naver.com>
2025-05-15 15:02:25 +0900

    feat: 로그인 엔드포인트 생성

--- src/main/java/com/example/springauth/controller/UserController.java
+++ src/main/java/com/example/springauth/controller/UserController.java
@@ -1,26 +1,37 @@
 package com.example.springauth.controller;
 
+import com.example.springauth.dto.request.ReqLoginDTO;
 import com.example.springauth.dto.request.ReqUserPostDTO;
+import com.example.springauth.dto.response.ResLoginDTO;
 import com.example.springauth.dto.response.ResUserPostDTO;
 import com.example.springauth.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
 public class UserController {
 
     private final UserService userService;
 
-    @PostMapping
+    @PostMapping("/signup")
     public ResponseEntity<ResUserPostDTO> createUser(
             @RequestBody ReqUserPostDTO dto) {
         ResUserPostDTO response = userService.save(dto);
         return ResponseEntity.ok(response);
     }
+
+    @PostMapping("/login")
+    public ResponseEntity<ResLoginDTO> login(
+            @RequestBody ReqLoginDTO dto) {
+        ResLoginDTO response = userService.login(dto);
+        return ResponseEntity.ok(response);
+    }
+
+
+
 }
